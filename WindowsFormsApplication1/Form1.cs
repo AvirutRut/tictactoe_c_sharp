deef8ac4f3973fdd6b6742f2961f11328aea737c
Rut <Rut@DESKTOP-R5SO8IJ>
2018-12-17 18:47:44 +0700

    Revert "h"
    This reverts commit 999477a519e600077e4e70979e912e464ee7bc1b.

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -972,27 +972,7 @@ namespace WindowsFormsApplication1
             return -1;
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            language = 0;
-            level = 1;
-            setDefault();
-            //enable english text
-            btnExit.Text = "ออก";
-            btnHumanToComputer.Text = "ผู้เล่น VS คอมพิวเตอร์";
-            btnHumanToHuman.Text = "ผู้เล่น1 VS ผู้เล่น2";
-            btnStart.Text = "เริ่มเกมส์";
-            btnUndo.Text = "ย้อนกลับ";
-            lblGameType.Text = "ตัวเลือก:";
-            lblLevel.Text = "ระดับเลเวล:";
-            lblPlayerX.Text = "ผู้เล่น x: ";
-            lblPlayerO.Text = "คอมพิวเตอร์: ";
-            lblResults.Text = "คะแนน:";
-            radioEasy.Text = "ระดับง่าย";
-            radioMedium.Text = "ระดับกลาง";
-            radioHard.Text = "ระดับยาก";
-            radioMedium.Checked = true;
-        }
+
     }
 
 }
